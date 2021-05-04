5a9596766fc4a64a460e1c1b00de769164817695
Alfredo de Morais <alfredneto@hotmail.com>
2021-05-04 20:47:44 -0300

    Add files via upload

---
+++ secao_2/aula_32_ex_1.py
@@ -0,0 +1,9 @@
+def ler_string(mensagem):
+    frase = input(mensagem)
+    print(frase)
+
+def ler_float(pergunta):
+    numero = input(pergunta)
+    numero = float(numero)
+    print(numero)
+    
---
+++ secao_2/aula_32_ex_2.py
@@ -0,0 +1,4 @@
+import aula_32_ex_1 as function
+
+function.ler_string('Digite seu nome: ')
+function.ler_float('Digite um número: ')
